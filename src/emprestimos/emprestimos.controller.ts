ed17eade891d8454188098295331ba8311586ab2
mateus-pa <mateuspinheiro1410@hotmail.com>
2026-07-07 17:24:24 -0300

    feat: elabora create empréstimos

--- src/emprestimos/emprestimos.controller.ts
+++ src/emprestimos/emprestimos.controller.ts
@@ -1,7 +1,25 @@
-import { Controller } from '@nestjs/common';
-import { EmprestimosService } from './emprestimos.service';
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { CurrentUser } from "../auth/decorators/current-user.decorator";
+import { Roles } from "../auth/decorators/roles.decorator";
+import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
+import { RolesGuard } from "../auth/guards/roles.guard";
+import CreateEmprestimosDto from "./dtos/create-emprestimos.dto";
+import { EmprestimosService } from "./emprestimos.service";
 
-@Controller('emprestimos')
+@Controller("emprestimos")
 export class EmprestimosController {
-  constructor(private readonly emprestimosService: EmprestimosService) {}
+	constructor(private readonly emprestimosService: EmprestimosService) {}
+
+	@Post()
+	@UseGuards(JwtAuthGuard, RolesGuard)
+	@Roles("ADMIN", "LEITOR")
+	async createEmprestimos(
+		@Body() createEmprestimosDto: CreateEmprestimosDto,
+		@CurrentUser() user: { id: string; role: string },
+	) {
+		return this.emprestimosService.createEmprestimos(
+			user.id,
+			createEmprestimosDto,
+		);
+	}
 }
